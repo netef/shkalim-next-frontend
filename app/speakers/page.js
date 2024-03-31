999deb4b6d9c83c81079da01b5f47ef8a7ac8e0e
netef <38144727+netef@users.noreply.github.com>
2024-03-31 15:04:19 +0300

    Update page.js

--- app/speakers/page.js
+++ app/speakers/page.js
@@ -2,10 +2,11 @@ import ProductsGrid from "@/utils/components/productsGrid/productsGrid";
 import { getAllSpeakers } from "@/utils/functions/apiCalls";
 
 export default async function SpeakersPage() {
-  const speakers = await getAllSpeakers();
+  // const speakers = await getAllSpeakers();
   return (
     <div>
-      <ProductsGrid products={speakers} />
+      <div>speakers</div>
+      {/* <ProductsGrid products={speakers} /> */}
     </div>
   );
 }
