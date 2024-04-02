0421329375ed73124579b0a7f26f5d91504bf4e1
netef <38144727+netef@users.noreply.github.com>
2024-04-02 11:24:04 +0300

    added firebase

--- app/speakers/page.js
+++ app/speakers/page.js
@@ -2,11 +2,11 @@ import ProductsGrid from "@/utils/components/productsGrid/productsGrid";
 import { getAllSpeakers } from "@/utils/functions/apiCalls";
 
 export default async function SpeakersPage() {
-  // const speakers = await getAllSpeakers();
+  const speakers = await getAllSpeakers();
   return (
     <div>
       <div>speakers</div>
-      {/* <ProductsGrid products={speakers} /> */}
+      <ProductsGrid products={speakers} />
     </div>
   );
 }
